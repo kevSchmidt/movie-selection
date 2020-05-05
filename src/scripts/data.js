0a5b35bc978de18fc8ae88e4027827fe7db51f81
Kevin <kev.saintigny.schmidt@gmail.com>
2020-05-05 16:26:43 +0200

    please kill me

---
+++ src/scripts/data.js
@@ -0,0 +1,33 @@
+const movies = [
+  {
+    id: 1,
+    title: "Into The Wild",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51sBtBUltwL._SY445_.jpg",
+    description: "",
+  },
+  {
+    id: 2,
+    title: "Ne Le Dis A Personne",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/51iVTqHDQDL._AC_SY445_.jpg",
+    description: "",
+  },
+  {
+    id: 3,
+    title: "Dances With Wolves",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/A1MmCKF4UGL._SL1500_.jpg",
+    description: "",
+  },
+  {
+    id: 4,
+    title: "The Boat That Rocked",
+    image:
+      "https://upload.wikimedia.org/wikipedia/en/e/e3/The_boat_that_rocked_poster.jpg",
+    description: "",
+  },
+];
+
+let data = JSON.stringify(movies);
+export { data };
