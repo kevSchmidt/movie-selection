47f504ae1e899fe8477e42c5dd31cdce700c485d
Kevin <kev.saintigny.schmidt@gmail.com>
2020-05-05 19:03:57 +0200

    Updates

--- scripts/data.js
+++ scripts/data.js
@@ -4,28 +4,32 @@ const movies = [
     title: "Into The Wild",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/51sBtBUltwL._SY445_.jpg",
-    description: "",
+    description:
+      "Based on a true story. After graduating from Emory University, Christopher McCandless abandoned his possessions, gave his entire savings account to charity, and hitchhiked to Alaska to live in the wilderness. Along the way, Christopher encounters a series of characters who shape his life.",
   },
   {
     id: 2,
     title: "Ne Le Dis A Personne",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/51iVTqHDQDL._AC_SY445_.jpg",
-    description: "",
+    description:
+      "An accidental discovery near a doctor's estate stirs up some painful memories eight years after his wife's hideous murder, and now, things are bound to take a turn for the unexpected. Does the good doctor know more than he's letting on?",
   },
   {
     id: 3,
     title: "Dances With Wolves",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/A1MmCKF4UGL._SL1500_.jpg",
-    description: "",
+    description:
+      "Lieutenant John Dunbar, assigned to a remote western Civil War outpost, befriends wolves and Indians, making him an intolerable aberration in the military.",
   },
   {
     id: 4,
     title: "The Boat That Rocked",
     image:
       "https://upload.wikimedia.org/wikipedia/en/e/e3/The_boat_that_rocked_poster.jpg",
-    description: "",
+    description:
+      "A band of rogue DJs that captivated Britain, playing the music that defined a generation and standing up to a government that wanted classical music, and nothing else, on the airwaves.",
   },
 ];
 
